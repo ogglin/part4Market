04e3f850a2d012ec857a14d23d648c329afb0cf9
Server <server.ares@gmail.com>
2019-12-24 22:36:31 +0300

    v0.0.0.1b - change paths

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   SERVER_URL_GET: 'https://api.part4.info:8443/get',
   SERVER_URL_PUT: 'https://api.part4.info:8443/put',
-  apiUrl: 'https://api.part4.info:8443',
+  apiUrl: 'https://api.part4.info:8443/upload',
   pathUpload: 'https://upload.part4.info/'
 };
